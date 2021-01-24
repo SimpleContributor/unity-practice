c5b69b3c41a24466ab6d2442b0e906d4a047a7e2
Caleb <calebcoecode@gmail.com>
2021-01-24 15:48:46 -0700

    Add Loop Examples for Unity

---
+++ Loop Examples/Assets/Scripts/Loops.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 3; i++ )
+        {
+            print("Loop: " + i);
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
