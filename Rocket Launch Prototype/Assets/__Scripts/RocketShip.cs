68d5e2b90046849f22b1e3aea8a8be0a0587b54e
Caleb <calebcoecode@gmail.com>
2021-02-09 19:42:03 -0700

    Add basic rocket with Keyboard Arrow movement. Need to add a landing point and obstacles to land on and fly around. Mess around with gravity and mass to make the rocket feel more realistic.

---
+++ Rocket Launch Prototype/Assets/__Scripts/RocketShip.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour
+{
+    Rigidbody myRigBody;
+    AudioSource myAudioSource;
+
+    public float shipSpeed = 100f;
+    public float rotSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigBody = GetComponent<Rigidbody>();
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+
+    
+    private void LateUpdate()
+    {
+        // Stops the ship from continuing to rotate when it hits or is hit my another object
+        transform.localEulerAngles = new Vector3(0, 0, transform.localEulerAngles.z);
+    }
+
+
+    private void FixedUpdate()
+    {
+        Fly();
+        Rotate();
+    }
+
+    public void Fly()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Flying");
+            myRigBody.AddRelativeForce(Vector3.up * shipSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myAudioSource.Play();
+        } else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            myAudioSource.Stop();
+        }
+    }
+
+    public void Rotate()
+    {
+        myRigBody.freezeRotation = true;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Debug.Log("Rotate Right");
+            //Quaternion rot = transform.rotation;
+            //rot = Quaternion.Euler(0, 0, 0 + rotSpeed);
+            //transform.rotation = rot;
+            transform.Rotate(-Vector3.forward * rotSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotSpeed);
+        }
+
+        myRigBody.freezeRotation = false;
+    }
+}
