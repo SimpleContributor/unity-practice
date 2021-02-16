e3771864d9105c98fb011d63f4d3980315ae2bbb
Caleb <calebcoecode@gmail.com>
2021-02-15 17:09:38 -0700

    Add Main Menu and Options Menu with working buttons. Need to hook up the volume slider to the master volume. Also need to create an in-game ESC menu where the user can adjust volume or go back to the main menu.

---
+++ Rocket Launch Prototype/Assets/__Scripts/CameraRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float spinSpeed = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Rocket Launch Prototype/Assets/__Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    GameController gameController;
+
+    private void Start()
+    {
+        gameController = GetComponent<GameController>();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //gameController.NextLevel();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+}
