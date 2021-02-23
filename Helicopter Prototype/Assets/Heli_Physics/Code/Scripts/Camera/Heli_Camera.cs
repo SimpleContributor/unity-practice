b2af836f2e7d799ef040f09af8217c79163ad392
Caleb <calebcoecode@gmail.com>
2021-02-23 15:49:08 -0700

    Add a camera interface and a simple follow camera. Still needs a first person camera.

---
+++ Helicopter Prototype/Assets/Heli_Physics/Code/Scripts/Camera/Heli_Camera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MyCode
+{
+    public class Heli_Camera : MonoBehaviour, IHeliCamera
+    {
+        public Rigidbody rb;
+        public Transform lookAtTarget;
+        public float height = 2f;
+        public float distance = -2f;
+
+        public float smoothSpeed = 1f;
+
+        private Vector3 wantedPos;
+        private Vector3 refVelocity;
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (rb)
+            {
+                UpdateCamera();
+            }
+        }
+
+
+        public void UpdateCamera()
+        {
+            //Vector3 heliPos = rb.position;
+            //Vector3 desiredPos = new Vector3(0, height, distance);
+            //transform.position = heliPos + desiredPos;
+
+            Vector3 flatForward = rb.transform.forward;
+            flatForward.y = 0f;
+            flatForward = flatForward.normalized;
+
+            // Desired position
+            wantedPos = rb.position + (flatForward * distance) + (Vector3.up * height);
+
+            // Position the camera
+            transform.position = Vector3.SmoothDamp(transform.position, wantedPos, ref refVelocity, smoothSpeed);
+            // transform.position = wantedPos;
+            transform.LookAt(lookAtTarget);
+        }
+    }
+}
