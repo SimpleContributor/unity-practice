3f00013e729ed975a110c95a0a6e68fc3004954d
Caleb <calebcoecode@gmail.com>
2021-02-24 12:12:09 -0700

    Start very basic 2D moon lander game. Main object is to safely land a spacecraft on a surface. Eventually want some danger involved. Maybe some flying obstacles or angry martians that destroy parts of the surface making it harder to land. Draining fuel to add some strategy. So far the ship has a surface to land on and basic thrust/rotation controls.

---
+++ Moon Lander/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float crashForce = 2.2f;
+
+    public float thrustForce = 20f;
+    public float rotForce = 2f;
+    float vInput, hInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleThrust();
+        HandleRotation();
+    }
+
+    void HandleThrust()
+    {
+        vInput = Input.GetAxisRaw("Vertical");
+        vInput = Mathf.Clamp01(vInput);
+        //rb.velocity = transform.up * vInput * thrustForce * Time.fixedDeltaTime;
+
+        rb.AddRelativeForce(new Vector2(0, vInput * thrustForce));
+    }
+
+    void HandleRotation()
+    {
+        hInput = Input.GetAxisRaw("Horizontal");
+        rb.transform.Rotate(Vector3.forward, hInput * rotForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Touching ground.");
+            Debug.Log(collision.relativeVelocity.magnitude);
+            if (collision.relativeVelocity.magnitude > crashForce)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
