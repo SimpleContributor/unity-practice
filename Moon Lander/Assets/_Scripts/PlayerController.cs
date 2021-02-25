185125a500fb1e79b43a81ce087285360da8ba32
Caleb <calebcoecode@gmail.com>
2021-02-25 11:13:29 -0700

    Add better art for the ship. Add a background. Change rotation to use Torque instead directly changing the transform.rotation. The torque coupled with a higher angular drag gives the ship a more spacey feel. Adjust the Rigidbody of the ship to be more realistic to the actual mars lander (mass, thrust force, gravity on the moon). Needs more levels and UI.

--- Moon Lander/Assets/_Scripts/PlayerController.cs
+++ Moon Lander/Assets/_Scripts/PlayerController.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    public Animator anims;
 
+    [Header("Rocket Properties")]
     public float crashForce = 2.2f;
-
     public float thrustForce = 20f;
     public float rotForce = 2f;
+
     float vInput, hInput;
+    bool thrusting = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.rotation = 0;
+
+        anims = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,24 +34,40 @@ public class PlayerController : MonoBehaviour
     void HandleThrust()
     {
         vInput = Input.GetAxisRaw("Vertical");
-        vInput = Mathf.Clamp01(vInput);
-        //rb.velocity = transform.up * vInput * thrustForce * Time.fixedDeltaTime;
-
-        rb.AddRelativeForce(new Vector2(0, vInput * thrustForce));
+        if (vInput > 0)
+        {
+            // If we are thrusting
+            rb.AddRelativeForce(new Vector2(0, vInput * thrustForce));
+            anims.SetBool("Thrusting", true);
+            thrusting = true;
+            Debug.Log("Thrusting!!!");
+        }
+        else if (vInput == 0 && thrusting)
+        {
+            anims.SetBool("Thrusting", false);
+            thrusting = false;
+            Debug.Log("No longer thrusting!!!");
+        }
     }
 
     void HandleRotation()
     {
         hInput = Input.GetAxisRaw("Horizontal");
-        rb.transform.Rotate(Vector3.forward, hInput * rotForce);
+        // Torque will apply a rotational force that will continue to spin without
+        // an opposing force. Angular drag will bring the rotation to a stop giving the 
+        // ship a more outerspace feel
+        rb.AddTorque(rotForce * hInput);
+
+        // Same behavior as rb.transform.Rotate except it should be used for 2D and .Rotate() for 3D
+        //rb.rotation += hInput * rotForce * Time.fixedDeltaTime;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-            Debug.Log("Touching ground.");
-            Debug.Log(collision.relativeVelocity.magnitude);
+            float collForce = (rb.mass / 2) * Mathf.Pow(collision.relativeVelocity.magnitude, 2);
+
             if (collision.relativeVelocity.magnitude > crashForce)
             {
                 Destroy(this.gameObject);
